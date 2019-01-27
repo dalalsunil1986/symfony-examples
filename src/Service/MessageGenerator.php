198b1584774cfe10819f288c1628d6e575f7d72f
Hatice Ergün <hatice.ergun@hacettepe.edu.tr>
2019-01-28 00:10:55 +0300

    RequestStack service added

--- src/Service/MessageGenerator.php
+++ src/Service/MessageGenerator.php
@@ -2,21 +2,32 @@
 
 namespace App\Service;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class MessageGenerator
 {
   /**
    * @var NameGenerator
    */
   private $nameGenerator;
+  /**
+   * @var RequestStack
+   */
+  private $requestStack;
 
-  public function __construct(NameGenerator $nameGenerator)
+  public function __construct(NameGenerator $nameGenerator, RequestStack $requestStack)
   {
     $this->nameGenerator = $nameGenerator;
+    $this->requestStack = $requestStack;
   }
 
   public function helloMessage()
   {
-    $message = 'Hello '.$this->nameGenerator->randomName();
+    $name = $this->requestStack->getCurrentRequest()->get('name');
+    if(empty($name)){
+      $name = $this->nameGenerator->randomName();
+    }
+    $message = 'Hello '.$name;
     return $message;
   }
 }
