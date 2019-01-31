0ba30fc388b5958df8f8d1f51d0605aa722e89c9
Hatice Ergün <hatice.ergun@hacettepe.edu.tr>
2019-01-31 23:08:56 +0300

    routings added

--- src/Controller/RoutingController.php
+++ src/Controller/RoutingController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RoutingController extends AbstractController
 {
@@ -70,11 +71,77 @@ class RoutingController extends AbstractController
   }
 
   /**
-   * @Route("/posts-listele/{page<\d+>?1903}")
+   * @Route("/posts-listele/{page<\d+>?1903}", name="post_listele_yeni")
    */
   //  ?1903  değeri ile default variable verdik.
   public function postListeleme2($page)
   {
     return new JsonResponse(['default post liste' => $page]);
+  }
+
+  /**
+   * @Route(
+   *   "/makaleler/{_locale}/{yil}/{slug}.{_format}",
+   *   name="makale",
+   *   defaults={"_format":"html"},
+   *   requirements={
+   *      "_locale": "en|tr",
+   *      "_format": "html|json",
+   *      "yil": "\d+"
+   *   }
+   * )
+   * @return JsonResponse
+   */
+  public function showMakale($_locale, $yil, $slug, $_format)
+  {
+    return new JsonResponse(['message' => implode("-", [
+      $_locale, $yil, $slug, $_format
+    ])]);
+  }
+
+  /**
+   * @Route("/generate-url")
+   */
+  public function urlUret()
+  {
+    $url = $this->generateUrl("makale", [ //showMakale kullanarak url oluştursun diye o fonksiyonun name değerini verdik.
+      '_locale' => 'en',
+      '_format' => 'html',
+      'yil' => 1903,
+      'slug' => 'kaliteli-hizmet'
+    ]);
+    return new JsonResponse(['url' => $url]);
+  }
+
+  /**
+   * @Route("/generate-url-service")
+   */
+  public function urlUret2(UrlGeneratorInterface $router)
+  {
+    $url = $router->generate("makale", [
+      '_locale' => 'en',
+      '_format' => 'html',
+      'yil' => 1903,
+      'slug' => 'kaliteli-hizmet-yeni-versiyon'
+    ]);
+    return new JsonResponse(['url' => $url]);
+  }
+
+  /**
+   * @Route("/generate-url-ornek")
+   */
+  public function ornek()
+  {
+    $url = $this->generateUrl("post_listele_yeni", [
+      'page' => 885,
+      'category' => 'saglik'  //ekstra  parametre gönderdik.
+    ]);
+
+    $fullUrl = $this->generateUrl("post_listele_yeni", [
+      'page' => 885,
+      'category' => 'saglik'
+    ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+    return new JsonResponse(['url' => $url, 'fullUrl' => $fullUrl]);
   }
 }
